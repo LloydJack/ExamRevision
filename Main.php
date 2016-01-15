0ad435757a79a4f3ed76a64f459f8ed24936f011
1303788 <lloydjack1@hotmail.co.uk>
2016-01-15 15:15:42 +0000

    Initial commit

---
+++ Main.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1303788
+ * Date: 15/01/2016
+ * Time: 15:13
+ */
+
+$juice = 'apple';
+echo "I drank some $juice juice<br />";
+echo 'I drank some $juice juice<br />';
+?>;
