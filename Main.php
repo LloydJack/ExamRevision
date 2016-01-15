54442168dafb118fde8c75d3b386f51dbeee39f1
1303788 <lloydjack1@hotmail.co.uk>
2016-01-15 15:31:04 +0000

    testerooni

--- Main.php
+++ Main.php
@@ -1,12 +1,18 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: 1303788
- * Date: 15/01/2016
- * Time: 15:13
- */
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>Deployment Test</title>
+</head>
+<body>
+<H1> It's  ALIVE!!!</H1>
 
+<?php
 $juice = 'apple';
 echo "I drank some $juice juice<br />";
 echo 'I drank some $juice juice<br />';
-?>;
+?>
+
+
+</body>
+</html>
